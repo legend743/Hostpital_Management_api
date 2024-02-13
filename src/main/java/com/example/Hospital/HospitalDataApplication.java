540a2ed6a7153c12c0a10e89d659e42dd2c80285
faisal <fmohd743@gmail.com>
2024-02-13 23:53:33 +0530

    swagger implemented

--- src/main/java/com/example/Hospital/HospitalDataApplication.java
+++ src/main/java/com/example/Hospital/HospitalDataApplication.java
@@ -3,7 +3,22 @@ package com.example.Hospital;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info=@Info(
+				title="Hospital-Api",
+				version="1.0..0",
+				description="Hospital Management apis",
+				contact=@Contact(
+						name="Mohd Faisal",
+						email="fmohd743@gmail.com")
+				)
+		
+		)
 public class HospitalDataApplication {
 
 	public static void main(String[] args) {
